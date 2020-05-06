7d33bcde1598825ca44b1be61f394402781e72dd
Your Name Here <youremailhere>
2020-05-06 15:10:06 -0400

    Finished

--- FindACrew/Views/PersonTableViewCell.swift
+++ FindACrew/Views/PersonTableViewCell.swift
@@ -12,8 +12,24 @@ class PersonTableViewCell: UITableViewCell {
     
     static let reuseIdentifier = "PersonCell"
 
+    var person: Person? {
+        didSet {
+            updateViews()
+        }
+    }
+    
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var birthYearLabel: UILabel!
 
+    
+    //Mark: - PRIVATE
+    
+    private func updateViews() {
+        guard let person = person else { return }
+        
+        nameLabel.text = person.name
+        heightLabel.text = "\(person.height) cm"
+        birthYearLabel.text = "Born \(person.birthYear)"
+    }
 }
